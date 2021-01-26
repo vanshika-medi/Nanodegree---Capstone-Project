7a8fc89ced1d10645329fb10d831fa5a14d85008
vanshika-medi <68374253+vanshika-medi@users.noreply.github.com>
2021-01-26 14:09:38 +0000

    Add files via upload

---
+++ starter_file/score1.py
@@ -0,0 +1,24 @@
+import json
+import numpy as np
+import os
+import joblib
+from pandas import DataFrame
+
+def init():
+    global model
+    #model_path = os.path.join(os.getenv('AZUREML_MODEL_DIR'), 'automlmodel.pkl')
+    model_path = os.path.join(os.getenv('AZUREML_MODEL_DIR'), 'model.pkl')
+    model = joblib.load(model_path)
+
+def run(data):
+    try:
+        data = json.loads(data)
+        test_data = data["data"]
+        data = DataFrame(test_data)
+        #data = np.array(json.loads(data))
+        result = model.predict(data)
+        # You can return any data type, as long as it is JSON serializable.
+        return result.tolist()
+    except Exception as e:
+        error = str(e)
+        return error
